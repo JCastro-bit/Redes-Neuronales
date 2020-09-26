b718992db79668dc47c71cc4f591aa075b539ae3
Javier Castro <javiercastro27@outlook.es>
2020-09-26 16:47:44 -0500

    made php components

---
+++ js/footer.php
@@ -0,0 +1,46 @@
+<div class="parallax-container valign-wrapper">
+        <div class="section no-pad-bot">
+            
+        </div>
+        <div class="parallax"><img src="noticias-bg.jpg" alt="Foto de fuente de universidad Cuauhtémoc"></div>
+    </div>
+
+    <footer class="page-footer teal">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <h5 class="white-text">Redes Neuronales - Javier Castro</h5>
+                    <p class="grey-text text-lighten-4">Portafolio de trabajos realizados en clase asi como de tareas</p>
+                    <p class="grey-text text-lighten-4">Mtro. Ricardo Ivan Castro Cocotl </p>
+                    <p class="grey-text text-lighten-4">Universidad Cuauhtémoc Plantel Guadalajara</p>
+                </div>
+
+                <div class="col l3 s12">
+                    <h5 class="white-text">Parciales</h5>
+                    <ul>
+                        <li><a class="white-text" href="#!">1er</a></li>
+                        <li><a class="white-text" href="#!">2do</a></li>
+                        <li><a class="white-text" href="#!">3ro</a></li>
+                        <li><a class="white-text" href="#!">Proyecto Final</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                Hecho por <a class="brown-text text-lighten-3" href="https://github.com/JCastro-bit" target="up">Javier
+                    Alejandro Castro Velázquez</a>
+            </div>
+        </div>
+    </footer>
+
+
+    <!--  Scripts-->
+    <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
+
+</body>
+
+</html>
---
+++ js/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Redes Neuronales - Javier Castro</title>
+
+
+    <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection" />
+</head>
+
+<body>
+    <nav class="teal" role="navigation">
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo">Redes Neuronales</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="index.html"><ion-icon size="large" name="home"></ion-icon></a></li>
+            </ul>
+
+            <ul id="nav-mobile" class="sidenav">
+                <li><a href="index.html"><ion-icon name="home"></ion-icon></a></li>
+            </ul>
+            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
+
+    <div id="index-banner" class="parallax-container">
+        <div class="section no-pad-bot">
+            <div class="container">
+                <br><br>
+                <h1 class="header center teal-text text-lighten-2">Redes Neuronales - Javier Castro</h1>
+                <div class="row center">
+                    <h5 class="header col s12 light">Portafolio de Trabajos en Clase</h5>
+                </div>
+                <br>
+            </div>
+        </div>
+        <div class="parallax"><img
+                src="https://images.unsplash.com/photo-1495592822108-9e6261896da8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+                alt="Unsplashed background img 1"></div>
+    </div>
+
